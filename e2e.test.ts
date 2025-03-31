747abbf07e40fab1e65b066f3a274471f444e0bd
pan <panwenwei123@gmail.com>
2025-03-31 11:25:02 +0800

    :wastebasket: remove e2e

--- e2e.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('Should return 200 response - /api/ping', async ({ page }) => {
-  const response = await page.goto('/api/ping')
-  expect(response?.status()).toBe(200)
-  expect(await response?.json()).toEqual({"message":"pong"})
-})
